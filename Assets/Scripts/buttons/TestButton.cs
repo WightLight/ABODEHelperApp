e49373bff7f02764810bd68e8ee8800f0016e84c
Ardeol <new0programmer@gmail.com>
2015-12-22 19:27:38 -0600

    buttons

---
+++ Assets/Scripts/buttons/TestButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButton : Button {
+    
+	void Start () {
+	    action = delegate(Button b) {
+	        Debug.Log("TEST");
+	    };
+	}
+	
+}
