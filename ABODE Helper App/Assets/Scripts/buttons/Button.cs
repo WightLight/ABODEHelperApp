f0a58bcb5d62c8032b6a2f8f531d359a07bcb132
ChromeWisp <chromewisp@gmail.com>
2016-06-15 16:08:35 -0500

    DICE AGAIN
    DELETED DICE SCRIPTS. REIMPOIRTED DICE SCRIPPS. PUT ON DICE AGAIN. NOT
    MISSING. GOOD.

---
+++ ABODE Helper App/Assets/Scripts/buttons/Button.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ *  The callback for the button.
+ *  @param b The button calling the callback.  Used if the button changes state.
+ */
+public delegate void ButtonAction(Button b);
+
+/**
+ *  To make a button, extend this class and assign the action as a delegate.
+ *  Essentially, make sure you have the following method defined.
+ * 
+    void Start () {
+	    action = delegate(Button b) {
+		//  IMPLEMENTATION HERE
+	    };
+	}
+ * 
+ *  The parameter is the button itself, in case the button's state changes as
+ *  a result of the callback.
+ *  ***************************************************************************/
+public class Button : MonoBehaviour {
+    public bool active = true;
+
+/**
+ *  Safe accessor for active state.
+ */
+	public bool Active {
+		get {  return active; }
+	}
+
+/*  Public Members
+ *  ==========================================================================*/
+/**
+ *  Presses the button if it is able to be pressed.
+ *  @param force Specify whether to force the button to be pressed.
+ */
+	public void Press(bool force = false) {
+		if(active || force)
+			action(this);
+	}
+
+/**
+ *  Activate the button.  Can be overridden if other side effects should be manifest.
+ */
+	public void Activate() {
+		active = true;
+	}
+
+/**
+ *  Deactivate the button.  Can be overridden if other side effects should be manifest.
+ */
+	public void Deactivate() {
+		active = false;
+	}
+
+/**
+ *  Toggle the active state.  Activate if inactive, deactivate if active.
+ */
+	public void Toggle() {
+		if(active)
+			Deactivate();
+		else
+			Activate ();
+	}
+
+/*  Private Members
+ *  ==========================================================================*/
+    protected ButtonAction action;
+}
---
+++ ABODE Helper App/Assets/Scripts/buttons/RollDice.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollDice : Button {
+	public const float VERTICAL_ROLL_FACTOR = 2;
+
+	public float maxRollForce = 100;
+    
+    public bool Rolling {
+		get {  return !rb.IsSleeping(); }
+	}
+    
+	void Start () {
+		//dice = GetComponent<MyDice>();
+		rb = GetComponent<Rigidbody>();
+		action = delegate(Button b) {
+			Roll();
+		};
+	}
+	
+	private void Roll() {
+	    if(!Rolling) {
+			rb.AddForce(new Vector3(Random.Range(-maxRollForce, maxRollForce), VERTICAL_ROLL_FACTOR * maxRollForce, Random.Range(-maxRollForce, maxRollForce)));
+			rb.AddTorque(new Vector3(Random.Range(-maxRollForce, maxRollForce), Random.Range(-maxRollForce, maxRollForce), Random.Range(-maxRollForce, maxRollForce)));
+		}
+	}
+
+	//private MyDice dice;
+	private Rigidbody rb;
+}
