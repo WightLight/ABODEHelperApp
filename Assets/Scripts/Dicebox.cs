2f72b6952051b9b188b346b15ef90882ea5b7dd8
ChromeWisp <chromewisp@gmail.com>
2016-06-24 03:00:50 -0500

    Roll All and New Die buttons fully functional!
    Added Dicebox script, with array to hold dice. New Die drops in a prefab
    d10 and puts it in the array. Roll All calls Roll() on every d10 in the
    array. Also got rid of those pesky messages from the buttons pertaining
    to not having animation transitions.

---
+++ Assets/Scripts/Dicebox.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dicebox : MonoBehaviour
+{
+    public Transform DieSpawnPoint;
+    public GameObject D10Prefab;
+    public GameObject[] Dice = new GameObject[10];
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    //If there's an empty space in the Dicebox, add a new d10.
+    public void NewD10()
+    {
+        for(int i = 0; i < Dice.Length; i++)
+        {
+            if(Dice[i] == null)
+            {
+                GameObject D10Clone = Instantiate(D10Prefab, DieSpawnPoint.position, DieSpawnPoint.rotation) as GameObject;
+                D10Clone.transform.parent = DieSpawnPoint;
+                Dice[i] = D10Clone;
+                break;
+            }
+        }
+    }
+
+    //Rolls every die in the Dicebox's Dice array.
+    //Better than Timothy's version! :3
+    public void RollAll()
+    {
+        foreach(GameObject die in Dice)
+        {
+            die.GetComponent<RollDice>().Roll();
+        }
+    }
+}
