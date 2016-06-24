2fccb888f5ade8d22043c9ead2c6c0115d3480fb
Auroratide <tf.auroratide@gmail.com>
2016-06-24 11:55:48 -0500

    Improved Array used for dice; added generic BoundedList class

--- Assets/Scripts/Dicebox.cs
+++ Assets/Scripts/Dicebox.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Dicebox : MonoBehaviour
 {
+	public const int MAX_DICE = 10;
+
     public Transform DieSpawnPoint;
     public GameObject D10Prefab;
-    public GameObject[] Dice = new GameObject[10];
+	public GameObject[] initialDice;
 
-	// Use this for initialization
+	public BoundedList<GameObject> Dice;
+
+/*  Unity API
+ *  ==========================================================================*/
 	void Start ()
     {
-
+		initDice();
 	}
 	
 	// Update is called once per frame
@@ -19,19 +24,16 @@ public class Dicebox : MonoBehaviour
         
 	}
 
+/*  Public Methods
+ *  ==========================================================================*/
     //If there's an empty space in the Dicebox, add a new d10.
     public void NewD10()
     {
-        for(int i = 0; i < Dice.Length; i++)
-        {
-            if(Dice[i] == null)
-            {
-                GameObject D10Clone = Instantiate(D10Prefab, DieSpawnPoint.position, DieSpawnPoint.rotation) as GameObject;
-                D10Clone.transform.parent = DieSpawnPoint;
-                Dice[i] = D10Clone;
-                break;
-            }
-        }
+		if(Dice.Count < Dice.Capacity) {
+			GameObject newDie = Instantiate(D10Prefab, DieSpawnPoint.position, DieSpawnPoint.rotation) as GameObject;
+			newDie.transform.parent = DieSpawnPoint;
+			Dice.Add(newDie);
+		}
     }
 
     //Rolls every die in the Dicebox's Dice array.
@@ -43,4 +45,12 @@ public class Dicebox : MonoBehaviour
             die.GetComponent<RollDice>().Roll();
         }
     }
+
+/*  Private Methods
+ *  ==========================================================================*/
+	private void initDice() {
+		Dice = new BoundedList<GameObject>(MAX_DICE);
+		foreach(GameObject die in initialDice)
+			Dice.Add(die);
+	}
 }
